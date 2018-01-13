6e58ae291b83e44e12e91bdf6e527ebcec343185
dima2018 <lukashindv@mail.ru>
2018-01-13 11:08:27 +0000

    fromVlad

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-/*var express = require('express');
-var app = express();
-app.listen(8080);
-
-app.get('/', function (req, res) {
-   res.sendfile('index.html');
-   console.log("Example app");
-});*/
-
-var express = require('express');  
+var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require("fs");
@@ -21,136 +12,174 @@ var ddd;
 var d = [];
 d[0] = 0;
 d[1] = 100;
- 
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.sendfile('index.html');      
-});
-app.get('/hello', function(req, res){
-    res.sendfile('hello.html');      
-});
-app.get('/goodBy', function(req, res){
-    res.sendfile('goodBy.html');      
-});
-app.get('/vote', function(req, res){
-     ddd = d.join(' ');        
-     res.write(ddd);
-     res.end(); 
- });
- app.post('/arhiv', function(req, res){
-     req.on('data', function(data){
-         
-         'use strict';
-            class User {
-         constructor(name){
-             this.name = name;                 
-         }
-         }
-         let Vasya = new User('vasya');
-         console.log(Vasya.name);
- 
-         
-var dat = new Date();
-var strDay = String(dat.getDate());
-var strManth = String(dat.getMonth()+1);
-var strYear = String(dat.getFullYear() - 2000);
-var DataToday = strDay+ strManth + strYear;
-var writeableStream = fs.createWriteStream(DataToday +".txt");
-writeableStream.write("Привет мир!");
-writeableStream.write("Продолжение записи \n");
-writeableStream.end("Завершение записи");
-         
-         //console.log(data.toString());         
-         var fff = data.toString() + ".txt";
-         var readableStream = fs.createReadStream(fff, "utf8");
-         readableStream.on("data", function(chunk){ 
-         res.write(chunk);
-         res.end(); 
-         });
-         
-     });
- });
 
-app.post('/client', function(req, res){
-    
-    i++;  
-    req.on('data', function(data){
-       console.log(data.toString());       
-         temp = data;
-         if(temp<=0)temp = 150-(temp*5);
-         else temp = temp*(-5)+ 150;
-     });
-     
-     req.on('end', function(){		
-	res.write('Данные с сервера' + i);
-        res.end();   
+
+app.get('/', function(req, res) {
+	res.sendfile('index.html');
+});
+
+/*
+app.get('/hello', function(req, res) {
+	res.sendfile('hello.html');
+});
+app.get('/goodBy', function(req, res) {
+	res.sendfile('goodBy.html');
+});
+app.get('/vote', function(req, res) {
+	ddd = d.join(' ');
+	res.write(ddd);
+	res.end();
+});
+*/
+
+
+
+app.post('/arhiv', function(req, res) {
+	req.on('data', function(data) {
+
+		'use strict';
+
+
+
+		var dat = new Date();
+		var strDay = String(dat.getDate());
+		var strManth = String(dat.getMonth() + 1);
+		var strYear = String(dat.getFullYear() - 2000);
+		var DataToday = strDay + strManth + strYear;
+		var writeableStream = fs.createWriteStream(DataToday + ".txt");
+
+		var fff = data.toString() + ".txt";
+		var readableStream = fs.createReadStream(fff, "utf8");
+
+
+
+		class User {
+			constructor(name) {
+				this.name = name;
+			}
+		}
+
+		let Vasya = new User('vasya');
+		console.log(Vasya.name);
+
+
+
+		writeableStream.write("Привет мир!");
+		writeableStream.write("Продолжение записи \n");
+		writeableStream.end("Завершение записи");
+
+
+
+		readableStream.on("data", function(chunk) {
+			res.write(chunk);
+			res.end();
+		});
+
 	});
-             
- 	cauntRequest++;	
-	
-		if(cauntRequest==19){
-                  cauntRequest=1; 
-                  d.length = 0;
-                  d[0] = 50;
-                  d[1] = 150;
-                }                    
-                    
-		if(cauntRequest==1)
-		{			
-		  d[2] = '100';
-		  d[3] = temp;
-		  
-		}
-		if(cauntRequest==2)
-		{			
-		  d[4] = '150';
-		  d[5] = temp;
-		}
-		
-		if(cauntRequest==3)
-		{			
-		  d[6] = '200';
-		  d[7] = temp;
-		}
-		if(cauntRequest==4)
-		{			
-		  d[8] = '250';
-		  d[9] = temp;
-		}
-		if(cauntRequest==5)
-		{			
-		  d[10] = '300';
-		  d[11] = temp;
-		}
-                if(cauntRequest==5)
-		{			
-		  d[12] = '350';
-		  d[13] = temp;
-		}
-                if(cauntRequest==5)
-		{			
-		  d[14] = '400';
-		  d[15] = temp;
-		}
-                if(cauntRequest==5)
-		{			
-		  d[16] = '450';
-		  d[17] = temp;
-		}
-                if(cauntRequest==5)
-		{			
-		  d[18] = '500';
-		  d[19] = temp;
-		}
-	    
 });
-/*app.get('/vote', function(req, res){
-     ddd = d.join(' ');        
-     res.write(ddd);
-     res.end();     
+
+app.post('/client', function(req, res) {
+
+	i++;
+	req.on('data', function(data) {
+		console.log(data.toString());
+		temp = data;
+		if (temp <= 0) temp = 150 - (temp * 5);
+		else temp = temp * (-5) + 150;
+	});
+
+	req.on('end', function() {
+		res.write('Данные с сервера' + i);
+		res.end();
+	});
+
+	cauntRequest++;
+
+	switch (cauntRequest) {
+		case 19:
+			cauntRequest = 1;
+			d.length = 0;
+			d[0] = 50;
+			d[1] = 150;
+			break;
+
+		case 1:
+			d[2] = '100';
+			d[3] = temp;
+			break;
+
+		case 2:
+			d[4] = '150';
+			d[5] = temp;
+			break;
+
+		case 3:
+			d[6] = '200';
+			d[7] = temp;
+			break;
+
+		case 4:
+			d[8] = '250';
+			d[9] = temp;
+			break;
+
+		case 5:
+			d[10] = '300';
+			d[11] = temp;
+			d[12] = '350';
+			d[13] = temp;
+			d[14] = '400';
+			d[15] = temp;
+			d[16] = '450';
+			d[17] = temp;
+			d[18] = '500';
+			d[19] = temp;
+			break;
+	}
+
+});
+
+
+
+/**
+ * page. vote
+
+app.get('/vote', function(req, res){
+		ddd = d.join(' ');
+		res.write(ddd);
+		res.end();
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*app.get('/:name', function(req, res){
+	 var name = req.params.name;  
+	 switch(name){
+	case 'temp':
+				res.sendfile('temp.html');break;         
+		case 'temp2':
+		res.sendfile('temp2.html');break;
+		default:
+		res.sendfile('test.html');break;
+		}    
 });*/
-
-
-
